a1dd089b6270ed7bacf427f18b1a026525570a84
B4rtux <35530977+B4rtux@users.noreply.github.com>
2022-06-02 17:02:04 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -129,7 +129,7 @@ export default class WearAHat {
 						}
 					}));
 				})
-				.catch(e => console.log.error("app", e));
+
 
 		}
 		if (this.egg === "rabbit") {
@@ -160,7 +160,7 @@ export default class WearAHat {
 						}
 					}));
 				})
-				.catch(e => console.log.error("app", e));
+
 		}
 
 	}
